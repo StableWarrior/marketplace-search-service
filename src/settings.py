fdba18beb00cf1f00b107beff34130cb0bc41638
Gleb Kibyakov <33168493+StableWarrior@users.noreply.github.com>
2026-06-02 17:19:23 +1000

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     postgres_password: str
     postgres_port: int
     postgres_username: str
-    kafka_brokers: str = "localhost:9092"
+    kafka_bootstrap_servers: str = "localhost:9092"
     kafka_topic_ads: str = "ads"
     kafka_consumer_group: str = "search-service"
     ad_service_url: str = "http://localhost:8002"
